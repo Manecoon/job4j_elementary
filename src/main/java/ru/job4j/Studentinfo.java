f91ff95617e72cf9269f3814608a4a35ff15990d
Manecoon <stas_on.85@mail.ru>
2022-03-22 10:48:50 +0300

    4.1. Разбор первой программы. [#159033]

--- src/main/java/ru/job4j/Studentinfo.java
+++ src/main/java/ru/job4j/Studentinfo.java
@@ -2,6 +2,7 @@ package ru.job4j;
 
 public class StudentInfo {
     public static void main(String[] args) {
+
         System.out.println("Stas Sh");
         System.out.println("21.02.1985");
     }
