e112a57d9bf0c7beda53e257c904254846f7e17d
Manecoon <stas_on.85@mail.ru>
2022-03-22 09:06:45 +0300

    job4j_elementary

--- src/test/java/ru/job4j/MainTest.java
+++ src/test/java/ru/job4j/MainTest.java
@@ -1,13 +1,23 @@
 package ru.job4j;
 
+<<<<<<< HEAD
 import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.*;
+=======
+import org.junit.Test;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+>>>>>>> a2f1a74 (job4j_elementary)
 
 public class MainTest {
     @Test
     public void whenActionThenResult() {
         int excepted = 1;
         int result = 1;
+<<<<<<< HEAD
         assertThat(result).isEqualTo(excepted);
+=======
+        assertThat(result, is(excepted));
+>>>>>>> a2f1a74 (job4j_elementary)
     }
 }
