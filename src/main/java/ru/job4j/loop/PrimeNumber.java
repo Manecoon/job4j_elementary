b8cee578a952ae1983bfbf4984b818ac4a5aff0c
Manecoon <stas_on.85@mail.ru>
2022-03-29 21:17:40 +0300

    5.6. Простые числа [#156314]

--- src/main/java/ru/job4j/loop/PrimeNumber.java
+++ src/main/java/ru/job4j/loop/PrimeNumber.java
@@ -1,13 +1,31 @@
 package ru.job4j.loop;
 
 public class PrimeNumber {
+
+    public static boolean check(int number) {
+        boolean prime = number != 1;
+        for (int i = 2; i < number; i++) {
+            if (number % i == 0) {
+                prime = false;
+                break;
+            }
+        }
+        return prime;
+    }
+
     public static int calc(int finish) {
         int count = 0;
-        for (int number = 2; number <= finish; number++) {
+        for (int number = 1; number <= finish; number++) {
+
             if (CheckPrimeNumber.check(number)) {
+                System.out.println(number);
                 count++;
             }
         }
         return count;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(calc(11));
     }
 }
--- src/test/java/ru/job4j/loop/PrimeNumberTest.java
+++ src/test/java/ru/job4j/loop/PrimeNumberTest.java
@@ -1,15 +1,17 @@
 package ru.job4j.loop;
 
-import org.junit.jupiter.api.Test;
-import static org.assertj.core.api.Assertions.assertThat;
+import org.junit.Test;
+
+import org.junit.Assert;
 
 public class PrimeNumberTest {
+
     @Test
     public void when5() {
         int finish = 5;
         int result = PrimeNumber.calc(finish);
         int expected = 3;
-        assertThat(result).isEqualTo(expected);
+        Assert.assertEquals(expected, result);
     }
 
     @Test
@@ -17,7 +19,7 @@ public class PrimeNumberTest {
         int finish = 11;
         int result = PrimeNumber.calc(finish);
         int expected = 5;
-        assertThat(result).isEqualTo(expected);
+        Assert.assertEquals(expected, result);
     }
 
     @Test
@@ -25,6 +27,6 @@ public class PrimeNumberTest {
         int finish = 2;
         int result = PrimeNumber.calc(finish);
         int expected = 1;
-        assertThat(result).isEqualTo(expected);
+        Assert.assertEquals(expected, result);
     }
 }
