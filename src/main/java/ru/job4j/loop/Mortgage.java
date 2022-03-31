5bf9b967c145e01b839b372aabecac64dee4a415
Manecoon <stas_on.85@mail.ru>
2022-03-31 10:59:19 +0300

    5.7. Ипотека [#156315]

--- src/main/java/ru/job4j/loop/Mortgage.java
+++ src/main/java/ru/job4j/loop/Mortgage.java
@@ -4,9 +4,13 @@ public class Mortgage {
     public static int year(double amount, int salary, double percent) {
         int year = 0;
         while (amount > 0) {
-            amount = (amount + amount * (percent / 100)) - salary;
+            amount = ((percent / 100) * amount + amount) - salary;
             year++;
         }
         return year;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(year(100, 70, 50));
     }
 }
