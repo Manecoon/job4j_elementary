3138682e74c9d21a1fe9a97f558b2c7077f56aba
Manecoon <stas_on.85@mail.ru>
2023-06-12 22:30:52 +0300

    4.2. Тернарное сравнение [#189  [#189 #279292]]

--- src/main/java/ru/job4j/condition/DummyBot.java
+++ src/main/java/ru/job4j/condition/DummyBot.java
@@ -1,17 +1,15 @@
 package ru.job4j.condition;
 
-public class DummyBot { public static String answer(String question) {
-    String rsl = "I don't know. Please, ask another question.";
-    if ("Hi, Bot.".equals(question)) {
-        /* заменить ... на правильный ответ rsl = "ответ по заданию". */
-        rsl = "Hi, SmartAss.";
-    } else if ("Bye.".equals(question)) { /* заменить ... на проверку, известен ли боту этот вопрос и он знает как на него ответить. */
-        /* заменить ... на правильный ответ rsl = "ответ по заданию". */
-        rsl = "See you later.";
+public class DummyBot {
+    public static String answer(String question) {
+        String rsl = "I don't know. Please, ask another question.";
+        if ("Hi, Bot.".equals(question)) {
+            rsl = "Hi, SmartAss.";
+        } else if ("Bye.".equals(question)) {
+            rsl = "See you later.";
+        }
+        return rsl;
     }
-    return rsl;
-}
-
 
     public static void main(String[] args) {
         String rsl = DummyBot.answer("Hi, Bot.");
