0ab536bf8eb223345044a927bea26e0692aff2a8
Manecoon <stas_on.85@mail.ru>
2023-06-13 15:03:57 +0300

    4.5. Отладка программы в IDEA [#159464  [#159464 #279725]]

--- src/test/java/ru/job4j/condition/SqAreaTest.java
+++ src/test/java/ru/job4j/condition/SqAreaTest.java
@@ -1,33 +1,25 @@
 package ru.job4j.condition;
 
-import org.junit.Assert;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.offset;
 
 public class SqAreaTest {
     @Test
     public void whenP6K2Square2() {
         int expected = 2;
         int p = 6;
-        double k = 2;
+        int k = 2;
         double out = SqArea.square(p, k);
-        Assert.assertEquals(expected, out, 0.01);
+        assertThat(out).isCloseTo(expected, offset(0.01));
     }
 
     @Test
-    public void whenP4K3Square2() {
-        double expected = 0.75;
+    public void whenP4K1Square1() {
+        int expected = 1;
         int p = 4;
-        double k = 3;
+        int k = 1;
         double out = SqArea.square(p, k);
-        Assert.assertEquals(expected, out, 0.01);
-    }
-
-    @Test
-    public void whenP8K4Square2() {
-        double expected = 2.56;
-        int p = 8;
-        double k = 4;
-        double out = SqArea.square(p, k);
-        Assert.assertEquals(expected, out, 0.01);
+        assertThat(out).isCloseTo(expected, offset(0.01));
     }
 }
