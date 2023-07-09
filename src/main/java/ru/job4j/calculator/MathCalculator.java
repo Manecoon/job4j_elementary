efccf9b186a140d27cd8df5535a31310032281b2
Manecoon <stas_on.85@mail.ru>
2023-07-09 09:48:28 +0300

    1. Чтение кода и базовые ошибки [#40205  [#40205 #428557]]

--- src/main/java/ru/job4j/calculator/MathCalculator.java
+++ src/main/java/ru/job4j/calculator/MathCalculator.java
@@ -1,44 +1,41 @@
-package ru.job4j.calculator;
-
-import static ru.job4j.math.MathFunction.*;
-
-public class MathCalculator {
-<<<<<<< HEAD
-    public static double sumAndMultiply(double first, double second) {
-        return sum(first, second)
-                + multiply(first, second);
-    }
-
-    public static double razAndDel(double first, double second) {
-        return raznost(first, second) + del(first, second);
-    }
-
-    public static double allSum(double first, double second) {
-        return sum(first, second) + multiply(first, second) + raznost(first, second)
-                + del(first, second);
-=======
-
-    public static double sumAndMultiply(double first, double second) {
-        return sum(first, second) + multiply(first, second);
-    }
-
-    public static double sumRaznostDel(double first, double second) {
-        return raznost(first, second) + delenie(first, second);
-    }
-
-    public static double sumVseh(double first, double second) {
-        return sum(first, second) + multiply(first, second) + raznost(first, second) + delenie(first, second);
->>>>>>> 7afd458 (1.1. Что такое import [#504773])
-    }
-
-    public static void main(String[] args) {
-        System.out.println("Результат расчета равен: " + sumAndMultiply(10, 20));
-<<<<<<< HEAD
-        System.out.println("Результат расчета равен: " + razAndDel(10, 20));
-        System.out.println("Результат расчета равен: " + allSum(10, 20));
-=======
-        System.out.println("Результат расчета равен: " + sumRaznostDel(10, 20));
-        System.out.println("Результат расчета равен: " + sumVseh(10, 20));
->>>>>>> 7afd458 (1.1. Что такое import [#504773])
-    }
-}
+//package ru.job4j.calculator;
+//
+//import static ru.job4j.math.MathFunction.*;
+//
+//public class MathCalculator {
+//
+//    public static double sumAndMultiply(double first, double second) {
+//        return sum(first, second)
+//                + multiply(first, second);
+//    }
+//
+//    public static double razAndDel(double first, double second) {
+//        return raznost(first, second) + del(first, second);
+//    }
+//
+//    public static double allSum(double first, double second) {
+//        return sum(first, second) + multiply(first, second) + raznost(first, second)
+//                + del(first, second);
+//
+//
+//        public static double sumAndMultiply ( double first, double second){
+//            return sum(first, second) + multiply(first, second);
+//        }
+//
+//        public static double sumRaznostDel ( double first, double second){
+//            return raznost(first, second) + delenie(first, second);
+//        }
+//
+//        public static double sumVseh ( double first, double second){
+//            return sum(first, second) + multiply(first, second) + raznost(first, second) + delenie(first, second);
+//        }
+//
+//        public static void main (String[]args){
+//            System.out.println("Результат расчета равен: " + sumAndMultiply(10, 20));
+//            System.out.println("Результат расчета равен: " + razAndDel(10, 20));
+//            System.out.println("Результат расчета равен: " + allSum(10, 20));
+//            System.out.println("Результат расчета равен: " + sumRaznostDel(10, 20));
+//            System.out.println("Результат расчета равен: " + sumVseh(10, 20));
+//        }
+//    }
+//}
