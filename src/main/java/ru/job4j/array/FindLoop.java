f095d4e5c265ccbf7d00f5215bdaf13e633065bb
Manecoon <stas_on.85@mail.ru>
2023-07-10 08:58:33 +0300

    1. Чтение кода и базовые ошибки [#40205  [#40205 #428557]]

--- src/main/java/ru/job4j/array/FindLoop.java
+++ src/main/java/ru/job4j/array/FindLoop.java
@@ -3,21 +3,23 @@ package ru.job4j.array;
 public class FindLoop {
     public static int indexOf(int[] data, int el) {
         int rst = -1;
-        for (int i = 0; i < data.length; i++) {
-            if ((data[i] == el)) {
-                rst = i;
+        for (int index = 0; index < data.length; index++) {
+            if (data[index] == el) {
+                rst = index;
                 break;
             }
         }
         return rst;
     }
 
-    public static void main(String[] args) {
-        int[] data = new int[]{5, 4, 3, 2};
-        System.out.println(indexOf(data, 10));
-    }
-
-    public static int indexInRange(int[] data, int min, int i, int i1) {
-        return min;
+    public static int indexInRange(int[] data, int el, int start, int finish) {
+        int rst = -1;
+        for (int index = start; index <= finish; index++) {
+            if (data[index] == el) {
+                rst = index;
+                break;
+            }
+        }
+        return rst;
     }
 }
